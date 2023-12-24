7b54d37f4fdc96f2f399a18a53b3a926129d1205
ismailkefokeris <ismailkefokeris@gmail.com>
2023-12-24 21:14:36 +0000

    Server connected to Front End...React Displaying Server Items

---
+++ frontend/src/components/backButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+
+const backButton = ({ destination = "/" }) => {
+    return <div className="flex">backButton</div>;
+};
+
+export default backButton;
