1a408360a0a0bbae80c78169218b9c4d3a81b968
ismailkefokeris <ismailkefokeris@gmail.com>
2023-12-24 02:07:47 +0000

    Server Mostly Complete

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,54 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+//https://medium.com/google-cloud/migrating-collections-from-mongodb-to-cloud-firestore-dfeff15f1c8
+//migrate from MongoDB to Firebase??
+import cors from "cors";
 
-require("dotenv").config;
+//Import Models
+import { Book } from "./models/bookModel.js";
+
+//Routes Import
+import booksRoute from "./routes/booksRoute.js";
+
+dotenv.config();
 
 const app = express();
 
-PORT = process.env.PORT;
+//Middleware for parsing request body
+app.use(express.json());
 
-app.listen(PORT, () => {});
+//CORS POLICY MiddleWare
+//Option 1: Allow all origins with default of Cors(*)
+// app.use(cors());
+//Option 2: Allow Custom Origins ---- This method is preffered as you have more control...
+app.use(
+    cors({
+        origin: "http://localhost:5000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
+
+const PORT = process.env.PORT;
+const mongoDBURL = process.env.mongoDBURL;
+
+app.get("/", (req, res) => {
+    console.log("Root...");
+    return res.status(254).send("Welcome to Book Store Project");
+});
+
+//Routes
+app.use("/books", booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("Database Connected...");
+        app.listen(PORT, () => {
+            console.log(`App Started at port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
---
+++ backend/models/bookModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        //Object that contains all the Features for our schema
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Book = mongoose.model("Book", bookSchema);
