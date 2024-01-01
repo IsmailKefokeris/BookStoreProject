7ee88d30d1d0725d33b8b7be01c5f57424b96c20
Ismail Kefokeris <ismailkefokeris@gmail.com>
2024-01-01 13:10:04 +0000

    Built in Card view for home page, able to switch between table and card view with ease and no reloading

---
+++ frontend/src/components/home/BookCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from "prop-types"
+import {PiBookOpenTextLight} from "react-icons/pi"
+import {BiUserCircle} from "react-icons/bi"
+import {AiOutlineEdit} from "react-icons/ai"
+import {BsInfoCircle} from "react-icons/bs"
+import {MdOutlineDelete} from "react-icons/md"
+import { Link } from 'react-router-dom'
+
+
+
+const BookCard = ({books}) => {
+  return (
+    <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+
+        {books.map((book) => {
+            return(
+                <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
+                    <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
+                    <h4 className='my-2 text-gray-500'>{book._id}</h4>
+
+                    <div className='flex justify-start items-center gap-x-2'>
+                        <PiBookOpenTextLight className='text-red-300 text-2xl'/>
+                        <h2 className='my-1'>{book.title}</h2>
+                    </div>
+                    <div className='flex justify-start items-center gap-x-2'>
+                        <BiUserCircle className='text-red-300 text-2xl'/>
+                        <h2 className='my-1'>{book.author}</h2>
+                    </div>
+                    <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                        <Link to={`/books/details/${book._id}`}>
+                            <BsInfoCircle className='text-green-300 text-2xl hover:text-black'/>
+                        </Link>
+                        <Link to={`/books/edit/${book._id}`}>
+                            <AiOutlineEdit className='text-orange-300 text-2xl hover:text-black'/>
+                        </Link>
+                        <Link to={`/books/delete/${book._id}`}>
+                            <MdOutlineDelete className='text-red-300 text-2xl hover:text-black'/>
+                        </Link>
+                    </div>
+                </div>
+            )
+        })}
+        
+
+    </div>
+  )
+}
+
+BookCard.propTypes = {
+    books: PropTypes.array.isRequired
+}
+
+export default BookCard
