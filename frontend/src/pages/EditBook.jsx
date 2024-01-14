0aa8ba177894b6ab7aa913892e3816be39fadee1
ismailkefokeris <ismailkefokeris@gmail.com>
2024-01-14 19:37:48 +0000

    Pulled all previous changes

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -9,69 +9,73 @@ import axios from "axios";
 
 import { useSnackbar } from "notistack";
 
-
-
 const EditBook = () => {
-    const[title, setTitle] = useState();
-    const[author, setAuthor] = useState();
-    const[publishYear, setPublishYear] = useState();
+    const [title, setTitle] = useState();
+    const [author, setAuthor] = useState();
+    const [publishYear, setPublishYear] = useState();
 
     const [loading, setLoading] = useState(false);
 
     const { id } = useParams();
 
-    const[error, setError] = useState("")
+    const [error, setError] = useState("");
 
-    const serverURL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+    const serverURL =
+        import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
 
     const navigate = useNavigate();
 
-    const {enqueueSnackbar} = useSnackbar();
+    const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
-      setLoading(true)
-      axios.get(`${serverURL}/books/${id}`)
-        .then((response) => {
-          setTitle(response.data.title)
-          setAuthor(response.data.author)
-          setPublishYear(response.data.publishYear)
-          setLoading(false)
-        })
-        .catch((error) => {
-          console.log(error)
-          // alert("An Error has occured check console...")
-          setLoading(false)
-        })
-    }, [])
+        setLoading(true);
+        axios
+            .get(`${serverURL}/books/${id}`)
+            .then((response) => {
+                setTitle(response.data.title);
+                setAuthor(response.data.author);
+                setPublishYear(response.data.publishYear);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                // alert("An Error has occured check console...")
+                setLoading(false);
+            });
+    }, []);
 
     const handleSubmit = () => {
         setLoading(true);
         axios
-            .put(`${serverURL}/books/${id}`, {title: title, author: author, publishYear: publishYear})
+            .put(`${serverURL}/books/${id}`, {
+                title: title,
+                author: author,
+                publishYear: publishYear,
+            })
             .then((response) => {
                 console.log(response);
                 setLoading(false);
                 navigate(`/`);
-                enqueueSnackbar("Book Updated Successfully!", {variant: "success"})
+                enqueueSnackbar("Book Updated Successfully!", {
+                    variant: "success",
+                });
             })
             .catch((error) => {
                 setLoading(false);
                 console.log(error);
-                setError(error.response.data.message)
-                enqueueSnackbar("Error updating book!", {variant: "error"})
-            })
+                setError(error.response.data.message);
+                enqueueSnackbar("Error updating book!", { variant: "error" });
+            });
     };
 
     return (
         <div className="p-4">
-
             <NavBar />
-            
+
             {loading ? (
                 <Spinner />
             ) : (
                 <div className="p-4 flex justify-between items-center flex-col">
-
                     <div className="flex justify-between items-center gap-3">
                         <BackButton />
 
@@ -80,27 +84,66 @@ const EditBook = () => {
 
                     <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
                         <div className="my-4">
-                            <label className="block text-gray-700 text-xl font-bold mb-2 mr-4" htmlFor="title">Title</label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-                            
+                            <label
+                                className="block text-gray-700 text-xl font-bold mb-2 mr-4"
+                                htmlFor="title"
+                            >
+                                Title
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="title"
+                                type="text"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
                         </div>
 
                         <div className="my-4">
-                            <label className="block text-gray-700 text-xl font-bold mb-2 mr-4" htmlFor="author">Author</label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="author" type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
-                            
+                            <label
+                                className="block text-gray-700 text-xl font-bold mb-2 mr-4"
+                                htmlFor="author"
+                            >
+                                Author
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="author"
+                                type="text"
+                                value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                            />
                         </div>
-                        
+
                         <div className="my-4">
-                            <label className="block text-gray-700 text-xl font-bold mb-2 mr-4" htmlFor="publishYear">Publish Year</label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="publishYear" type="number" min="1500" max="2024" step="1" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} />
-                            
+                            <label
+                                className="block text-gray-700 text-xl font-bold mb-2 mr-4"
+                                htmlFor="publishYear"
+                            >
+                                Publish Year
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="publishYear"
+                                type="number"
+                                min="1500"
+                                max="2024"
+                                step="1"
+                                value={publishYear}
+                                onChange={(e) => setPublishYear(e.target.value)}
+                            />
                         </div>
 
-                        <p id="formError" className="text-red-700 font-bold">{error}</p>
+                        <p id="formError" className="text-red-700 font-bold">
+                            {error}
+                        </p>
 
-                        <Link onClick={handleSubmit} className="flex justify-end" type="submit">
-                            <TiTick className="text-2xl text-green-800"/>
+                        <Link
+                            onClick={handleSubmit}
+                            className="flex justify-end"
+                            type="submit"
+                        >
+                            <TiTick className="text-2xl text-green-800" />
                         </Link>
                     </div>
                 </div>
